5df859b35aa07dd4cbd75e1ab4fea4a2ca89afd0
promarg <elbouslamti.omar@gmail.com>
2019-09-09 13:01:21 +0200

    get popular movie data

--- src/app/api-service.service.ts
+++ src/app/api-service.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApiServiceService {
 
-  constructor() { }
+export class ApiServiceService {
+  baseUrl: string;
+  apiKey: string;
+  language: string;
+  region: string;
+
+  constructor(public http : HttpClient) {
+  this.baseUrl = "https://api.themoviedb.org/3";
+  this.apiKey = "b6e974e18e53b8a8bfb9e9827e149190"
+
+   }
+
+  getOverview(): Observable<any>{
+    return this.http.get<any>(`${this.baseUrl}/movie/550?api_key=${this.apiKey}`)
+  }
+
+  getPopular(): Observable<any>{
+    return this.http.get<any>(`${this.baseUrl}/movie/popular?page=1&api_key=${this.apiKey}`)
+  }
+
+
+
+
+
+
+
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiServiceService } from '../api-service.service';
+import { Subscriber } from 'rxjs';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+   title = "AnguMovies";
+  
+   overview: string;
+
+   id: number;   
+   image1: string;
+   image2: string;
+   popularity: number;
+   vote: number;
+   release: Date;
+
+  constructor(public apiMovies: ApiServiceService) { 
+
+  }
 
   ngOnInit() {
-  }
+    this.apiMovies.getOverview().subscribe(result =>{
+      this.overview = result.overview;
+    });
+
+    this.apiMovies.getPopular().subscribe(result =>{
+      this.id = result.results.id;
+      this.image1 = result.poster_path;
+      this.image2 = result.backdrop_path;
+      this.popularity = result.popularity;
+      this.vote = result.vote_average;
+      this.release = result.release_date;
+    })
+  };
 
 }
